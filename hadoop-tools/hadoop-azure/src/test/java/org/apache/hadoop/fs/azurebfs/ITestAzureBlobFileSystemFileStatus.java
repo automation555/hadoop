74e84f8ce7cc9a3968118faa700d626a53a6cdaa
rajashree patil <rajashree.patil@embold.io>
2021-07-05 15:35:04 +0530

    commitMessage : HADOOP-16752. assertion failure in ITestAzureBlobFileSystemFileStatus

--- hadoop-tools/hadoop-azure/src/test/java/org/apache/hadoop/fs/azurebfs/ITestAzureBlobFileSystemFileStatus.java
+++ hadoop-tools/hadoop-azure/src/test/java/org/apache/hadoop/fs/azurebfs/ITestAzureBlobFileSystemFileStatus.java
@@ -67,7 +67,7 @@ public class ITestAzureBlobFileSystemFileStatus extends
 
     String errorInStatus = "error in " + fileStatus + " from " + fs;
 
-    if (!getIsNamespaceEnabled(fs)) {
+    if (!fs.getIsNamespaceEnabled()) {
       assertEquals(errorInStatus + ": owner",
               fs.getOwnerUser(), fileStatus.getOwner());
       assertEquals(errorInStatus + ": group",
@@ -138,6 +138,6 @@ public class ITestAzureBlobFileSystemFileStatus extends
     assertTrue("lastModifiedTime should be after minCreateStartTime",
         minCreateStartTime < lastModifiedTime);
     assertTrue("lastModifiedTime should be before createEndTime",
-        createEndTime > lastModifiedTime);
+        createEndTime >= lastModifiedTime);
   }
 }
